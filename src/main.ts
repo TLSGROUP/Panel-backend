63f3410df89b1e723bc7f74ee9c5569436a4ec76
Ruslan <vedexpert@live.ru>
2026-02-06 18:04:55 +0100

    add share static file

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { NestFactory } from '@nestjs/core'
 import { raw } from 'express'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+	// Serve static files from uploads directory
+	app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+		prefix: '/uploads/',
+	})
 
 	app.setGlobalPrefix('api', {
 		exclude: [
